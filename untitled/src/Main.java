715d1c15903acf8cdfee10415671f4d573ce8c61
mohamedhasan-coder <mohamedhasan8403@gmail.com>
2026-09-22 08:56:04 +0530

    Day 2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name : ");
+        name = sc.nextLine();
+        System.out.println("Your name is :" + name);
+
+        sc.close();
     }
 }
